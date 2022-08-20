922f47b4244f2305930827d1e58bd59f526f6762
dmendezcreativ <111564830+dmendezcreativ@users.noreply.github.com>
2022-08-20 23:27:51 +0200

    Update user.ts
    Bugfix: Change-Password did'nt work

--- src/app/users/user.ts
+++ src/app/users/user.ts
@@ -40,6 +40,7 @@ export class User extends IdEntity {
 
     async hashAndSetPassword(password: string) {
         this.password = (await import('password-hash')).generate(password);
+        this.save();
     }
     async passwordMatches(password: string) {
         return !this.password || (await import('password-hash')).verify(password, this.password);
